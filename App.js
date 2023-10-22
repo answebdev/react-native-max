aca897d0c84995baea723ce94915d03ac8f44854
ANS <36783010+answebdev@users.noreply.github.com>
2023-10-21 19:51:19 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
+// Video: https://www.youtube.com/watch?v=VozPNrt-LfE
+// Starting project: https://github.com/academind/react-native-practical-guide-code/blob/02-basics/extra-files/starting-project.zip
+// Attachments & code snapshots: https://github.com/academind/react-native-practical-guide-code
+// goal.png File: https://github.com/academind/react-native-practical-guide-code/blob/02-basics/extra-files/goal.png
+
 // Stopped at 1:30:03
 
 export default function App() {
