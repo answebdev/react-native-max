3b4cf5cb878b33c08a9cfb18bcefa15298aea771
ANS <36783010+answebdev@users.noreply.github.com>
2023-10-28 10:42:39 -0700

    add Delete functionality

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import GoalInput from './components/GoalInput';
 // Attachments & code snapshots: https://github.com/academind/react-native-practical-guide-code
 // goal.png File: https://github.com/academind/react-native-practical-guide-code/blob/02-basics/extra-files/goal.png
 
-// Stopped at 2:20:40
+// Stopped at 2:46:57
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
@@ -16,22 +16,43 @@ export default function App() {
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      { text: enteredGoalText, key: Math.random().toString() }
+      { text: enteredGoalText, id: Math.random().toString() }
     ]);
+  }
+
+  // Delete an item.
+  // Receive the ID of the item that is to be deleted, i.e., pass the 'id' in as a function parameter.
+  function deleteGoalHandler(id) {
+    // Take the old state, but remove an item; then, return the updated state.
+    setCourseGoals((currentCourseGoals) => {
+      // Filter returns a new array, which is the old array minus all the items we filtered out.
+      // 'Filter' itself takes a function, which has to return 'true' or 'false'.
+      // If it returns 'true', an item is kept.
+      // If it returns 'false', an item is removed.
+      // We want to return 'false' is our goal has 'id'
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
     <View style={styles.appContainer}>
       <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
-        <FlatList data={courseGoals} renderItem={(itemData) => {
-          return (
-            <GoalItem text={itemData.item.text} />
-          );
-        }}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
           keyExtractor={(item, index) => {
             return item.id;
           }}
+        // keyExtractor={(item) => item.id} 
         />
       </View>
     </View>
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,28 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
     return (
-        <View style={styles.goalItem}>
-            <Text style={styles.goalText}>{props.text}</Text>
-        </View>
+        // To make an item pressable (e.g., press/tap a button to delete the item), you have to let React Native know.
+        // And you let React Native know by wrapping the item inside the 'Pressable' component (which you need to import from React-Native).
+        // Once you then wrap it in the Pressable component, the item then becomes pressable.
+        // And now, whenever we press an item inside of <Pressable />, the'onPress' prop will trigger the function that we provide to it (e.g., 'onDeleteItem').
+
+        // The function that deletes an item is in App.js. It needs the ID of the item to be deleted,
+        // but it does not have access to it, since it is here in this component.
+        // To help provide the 'id' to the delete function in App.js, we use 'bind' (see below) (this is just one way of solving this issue).
+        // Be sure to also pass in an ID prop in App.js in the <GoalItem /> component, e.g. <GoalItem id={itemData.item.id} />
+
+        // Call the built-in 'bind' method on thw function, which we get through on the 'onDeleteItem' prop.
+        // 'bind' allows you to pre-configure a function for future execution.
+        // The first value you pass to 'bind' sets the 'this' keyword in the to-be executed function.
+        // Since we don't care about that here, we'll just set it to 'this'.
+        // The second argument you pass to 'bind' will be the first parameter received by the to-be called function ('onDeleteItem').
+        // In this case, it should be the ID of the goal item that should be deleted.
+        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+            <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{props.text}</Text>
+            </View>
+        </Pressable>
     );
 }
 
